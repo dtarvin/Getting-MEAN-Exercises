a99b066186645eed3ad468e48ca9390c5ff57c78
a161120 <david.tarvin@physiciansmutual.com>
2019-10-22 08:56:42 -0500

    add post and update to apis

--- loc8r/app_api/controllers/reviews.js
+++ loc8r/app_api/controllers/reviews.js
@@ -1,63 +1,177 @@
 const mongoose = require('mongoose');
 const Loc = mongoose.model('Location');
 
+const doSetAverageRating = (location) => {
+	if (location.reviews && location.reviews.length > 0) {
+		const count = location.reviews.length;
+		const total = location.reviews.reduce((acc, {rating}) => {
+			return acc + rating;
+		}, 0);
+		location.rating = parseInt(total / count, 10);
+		location.save(err => {
+			if (err) {
+				console.log(err);
+			} else {
+				console.log(`Average rating updated to ${location.rating}`);
+			}
+		});
+	}
+};
+
+const updateAverageRating = (locationId) => {
+	Loc.findById(locationId)
+		.select('rating reviews')
+		.exec((err, location) => {
+			if (!err) {
+				doSetAverageRating(location);
+			}
+		});
+};
+
+const doAddReview = (req, res, location) => {
+	if (!location) {
+		res
+			.status(404)
+			.json({"message": "Location not found"});
+	} else {
+		const {author, rating, reviewText} = req.body;
+		location.reviews.push({
+			author,
+			rating,
+			reviewText
+		});
+		location.save((err, location) => {
+			if (err) {
+				res
+					.status(400)
+					.json(err);
+			} else {
+				updateAverageRating(location._id);
+				const thisReview = location.reviews.slice(-1).pop();
+				res
+					.status(201)
+					.json(thisReview);
+			}
+		})
+	}
+}
+
 const reviewsCreate = (req, res) => { 
-    res
-      .status(200)
-      .json({"status" : "success"});
+	res
+		.status(200)
+		.json({"status" : "success"});
 };
 const reviewsReadOne = (req, res) => { 
-    Loc
-      .findById(req.params.locationid)
-      .select('name reviews')
+	Loc
+		.findById(req.params.locationid)
+		.select('name reviews')
 
-      .exec((err, location) => {
-          if (!location) {
-              return res
-                .status(404)
-                .json({
-                    "message": "location not found"
-                });
-          } else if (err) {
-              return res
-                .status(400)
-                .json(err);
-          }
-          if (location.reviews && location.reviews.length > 0) {
-              const review = location.reviews.id(req.params.reviewid);
-              if (!review) {
-                  return res
-                    .status(400)
-                    .json({
-                        "message": "review not found"
-                    });
-              } else {
-                  response = {
-                      location : {
-                          name: location.name,
-                          id: req.params.locationid
-                      },
-                      review
-                  };
-                  return res
-                    .status(200)
-                    .json(response);
-              }
-          } else {
-              return res
-                .status(404)
-                .json({
-                    "message": "no reviews found"
-                });
-          }
-      });
+		.exec((err, location) => {
+			if (!location) {
+				return res
+					.status(404)
+					.json({
+						"message": "location not found"
+					});
+			} else if (err) {
+				return res
+					.status(400)
+					.json(err);
+			}
+			if (location.reviews && location.reviews.length > 0) {
+				const review = location.reviews.id(req.params.reviewid);
+				if (!review) {
+					return res
+						.status(400)
+						.json({
+							"message": "review not found"
+						});
+				} else {
+					response = {
+						location : {
+							name: location.name,
+							id: req.params.locationid
+						},
+						review
+					};
+					return res
+						.status(200)
+						.json(response);
+				}	
+			} else {
+				return res
+					.status(404)
+					.json({
+						"message": "no reviews found"
+					});
+			}
+    });
 };
-const reviewsUpdateOne = (req, res) => { };
+
+const reviewsUpdateOne = (req, res) => { 
+	if (!req.params.locationid || !req.params.reviewid) {
+		return res
+			.status(404)
+			.json({
+				"message": "Not found, locationid and reviewid are both required"
+			});
+	}
+	Loc
+		.findById(req.params.locationid)
+		.select('reviews')
+		.exec((err, location) => {
+			if (!location) {
+				return res
+					.status(404)
+					.json({
+						"message": "Location not found"
+					});
+			} else if (err) {
+				return res
+					.status(400)
+					.json(err);
+			}
+			if (location.reviews && location.reviews.length > 0) {
+				const thisReview = location.reviews.id(req.params.reviewid);
+				if (!thisReview) {
+					res
+						.status(404)
+						.json({
+							"message": "Review not found"
+						});
+				} else {
+					thisReview.author = req.body.author;
+					thisReview.rating = req.body.rating;
+					thisReview.reviewText = req.body.reviewText;
+					location.save((err, location) => {
+						if (err) {
+							res
+								.status(404)
+								.json(err);
+						} else {
+							updateAverageRating(location._id);
+							res
+								.status(200)
+								.json(thisReview);
+						}
+					});
+				}
+			} else {
+				res
+					.status(404)
+					.json({
+						"message": "No review to update"
+					});
+			}
+		}
+	);
+};
+
 const reviewsDeleteOne = (req, res) => { };
 
 module.exports = {
-    reviewsCreate,
-    reviewsReadOne,
-    reviewsUpdateOne,
-    reviewsDeleteOne
+	reviewsCreate,
+	reviewsReadOne,
+	reviewsUpdateOne,
+	reviewsDeleteOne
 };
--- loc8r/app_api/models/db.js
+++ loc8r/app_api/models/db.js
@@ -4,40 +4,40 @@ const dbURI = 'mongodb://localhost/Loc8r';
 mongoose.connect(dbURI, {useNewUrlParser: true});
 
 mongoose.connection.on('connected', () => {
-    console.log(`Mongoose connected to ${dbURI}`);
+	console.log(`Mongoose connected to ${dbURI}`);
 });
 
 mongoose.connection.on('error', err => {
-    console.log('Mongoose connection error:', err);
+  console.log('Mongoose connection error:', err);
 });
 
 mongoose.connection.on('disconnected', () => {
-    console.log('Mongoose disconnected');
+  console.log('Mongoose disconnected');
 });
 
 const gracefulShutdown = (msg, callback) => {
-    mongoose.connection.close( () => {
-        console.log(`Mongoose disconnected through ${msg}`);
-        callback();
-    });
+	mongoose.connection.close( () => {
+		console.log(`Mongoose disconnected through ${msg}`);
+		callback();
+	});
 };
 
 process.once('SIGUSR2', () => {
-    gracefulShutdown('nodemon restart', () => {
-        process.kill(process.pid, 'SIGUSR2');
-    });
+	gracefulShutdown('nodemon restart', () => {
+		process.kill(process.pid, 'SIGUSR2');
+	});
 });
 
 process.on('SIGINT', () => {
-    gracefulShutdown('app termination', () => {
-        process.exit(0);
-    });
+	gracefulShutdown('app termination', () => {
+		process.exit(0);
+	});
 });
 
 process.on('SIGTERM', () => {
-    gracefulShutdown('Heroku app shutdown', () => {
-        process.exit(0);
-    });
+	gracefulShutdown('Heroku app shutdown', () => {
+		process.exit(0);
+	});
 });
 
 require('./locations');
