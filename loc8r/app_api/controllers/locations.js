b7ef61117c91f1d916475db472e80f89dd2fe7aa
a161120 <david.tarvin@physiciansmutual.com>
2019-10-23 08:21:18 -0500

    finish chapter 6

--- loc8r/app_api/controllers/locations.js
+++ loc8r/app_api/controllers/locations.js
@@ -167,7 +167,30 @@ const locationsUpdateOne = (req, res) => {
 	);
 };
 
-const locationsDeleteOne = (req, res) => { };
+const locationsDeleteOne = (req, res) => { 
+  const {locationid} = req.params;
+  if (locationid) {
+    Loc
+      .findByIdAndRemove(locationid)
+      .exec((err, location) => {
+        if (err) {
+          return res
+            .status(404)
+            .json(err);
+        }
+        res
+          .status(204)
+          .json(null);
+      }
+    );
+  } else {
+    res
+      .status(404)
+      .json({
+        "message": "No location"
+      });
+  }
+};
 
 module.exports = {
     locationsListByDistance,
